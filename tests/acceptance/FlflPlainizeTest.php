fd82bd02277422a319a53223ddeece9c84d0d3a0
Calpo <calpo22@gmail.com>
2014-04-20 10:35:50 +0900

    fix failed tests.

--- tests/acceptance/FlflPlainizeTest.php
+++ tests/acceptance/FlflPlainizeTest.php
@@ -3,13 +3,13 @@
  * This file is part of the flfl-plainize.
  */
 
-namespace FlflPlainize\Tests\acceptance;
+namespace FlflPlainize\Tests\Acceptance;
 
 
 /**
  * @coversNothing
  */
-class BinFlflPlainizeTest extends \PHPUnit_Framework_TestCase
+class FlflPlainizeTest extends \PHPUnit_Framework_TestCase
 {
     public function setUp()
     {
@@ -39,9 +39,8 @@ class BinFlflPlainizeTest extends \PHPUnit_Framework_TestCase
         list($date, $firstItem, $secondItem) = explode("\t", $output[0]);
 
         // comparing by timestamp, to avoid dependence on timezone setting.
-        $this->assertEquals(strtotime('2014-03-18 17:25:38'), strtotime($date));
+        $this->assertEquals(strtotime('2014-03-18T17:25:38+0900'), strtotime($date));
         $this->assertEquals('f111', $firstItem);
         $this->assertEquals('z111', $secondItem);
     }
 }
- 
